9c8dcf3f25e9a9bd330301482061657a6abc8e5a
David <IFCE@IFCEs-MacBook-Pro-2.local>
2017-09-12 09:10:35 -0300

    Initial Commit

---
+++ Avenue/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Avenue
+//
+//  Created by IFCE on 12/09/17.
+//  Copyright © 2017 RenataFG. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
